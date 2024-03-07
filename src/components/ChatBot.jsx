e3fd9338505ff4ca635a5d2f5bd2263fb22e8a55
MickaelMaujean <mickael.maujean@hotmail.fr>
2024-03-07 12:27:30 +0100

    AI bot placeholder

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { FaRobot } from "react-icons/fa6";
 
 function ChatBot() {
+	const handleAIbot = () => {};
+
 	return (
 		<div id="chatbot" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
 			<hr class="border-b my-8 border-gray-800" />
@@ -19,8 +21,17 @@ function ChatBot() {
 						type="text"
 						name="support"
 						className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-						placeholder="Ask your question"
+						placeholder="Your AI bot is coming soon, stay tuned !"
 					/>
+				</div>
+				<div className="mt-2">
+					<button
+						type="submit"
+						onClick={handleAIbot}
+						className="rounded-md bg-gray-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						Send
+					</button>
 				</div>
 			</div>
 		</div>
